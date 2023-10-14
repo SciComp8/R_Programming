97e7bf4489c3ffae82b887097a3d8738f0a0003a
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-14 11:40:23 -0400

    Create lchoose.R

---
+++ lchoose.R
@@ -0,0 +1,5 @@
+# The functions choose and lchoose return binomial coefficients and the logarithms of their absolute values. Note that choose(n, k) is defined for all real numbers n and integer k.
+log(choose(2,1))
+# [1] 0.6931472
+lchoose(2,1)
+# [1] 0.6931472
