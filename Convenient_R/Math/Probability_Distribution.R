b338fe5aae096be2b08e19775950e05aa8ff6b2b
Anni <90419301+SciComp8@users.noreply.github.com>
2024-12-08 12:36:21 -0500

    Update Probability_Distribution.R

--- Convenient_R/Math/Probability_Distribution.R
+++ Convenient_R/Math/Probability_Distribution.R
@@ -19,3 +19,7 @@ qnorm(0.95, 6, 9)
 # Assume X ~ Uniform. Plot the density function
 theta=seq(0,1,.01)
 plot(theta, dbeta(theta,1,1), tyle='l')
+
+# Assume X ~ Beta(4, 3). Plot the density function
+theta=seq(0,1,.01)
+plot(theta, dbeta(theta,4,3), tyle='l')
