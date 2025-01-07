319b145e0d24ed9acd1a602d23c8a0cd5e85ac37
Anni <90419301+SciComp8@users.noreply.github.com>
2025-01-07 16:55:15 -0500

    Delete Plot/*Reproducible_Jitter.R

--- Plot/*Reproducible_Jitter.R
+++
@@ -1,7 +0,0 @@
-library(ggplot2)
-jitter_set <- position_jitter(height = 0.1, seed = 689) # A random seed to make the jitter reproducible
-ggplot(
-  data = df_med,
-  mapping = aes(x = rank, y = condition)) +     
-  geom_point(position = jitter_set, alpha = 0.5, color = "#7c20c1") + 
-  scale_x_continuous(breaks = min(df_med$rank):max(df_med$rank))
