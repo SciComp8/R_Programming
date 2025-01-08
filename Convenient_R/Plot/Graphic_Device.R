fc3250b15eb58ae00903dbaf20be3762727d3263
Anni <90419301+SciComp8@users.noreply.github.com>
2025-01-08 11:34:49 -0500

    Create Graphic_Device.R

---
+++ Convenient_R/Plot/Graphic_Device.R
@@ -0,0 +1,4 @@
+# dev.cur(): Shows the currently active graphics device.
+# dev.list(): Lists all open graphics devices and their IDs.
+# dev.off(): Closes the currently active graphics device.
+# dev.set(which = 2): Switches to a specific device (e.g., which = 2 switches to RStudioGD 2).
