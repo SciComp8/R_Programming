6097edd1669ffd7cfd810f19a5c067af9d813b50
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-03-29 10:57:10 -0400

    Update !%in%.R

--- Convenient_R/!%in%.R
+++ Convenient_R/!%in%.R
@@ -3,3 +3,6 @@ probe.keep.1 <- !(featureNames(norm.set) %in% anndata.450k$Name[anndata.450k$chr
 probe.keep.2 <- !(featureNames(norm.set.filter) %in% xReactiveProbes$norm.set.filter)
 
 uniq_sample_1 <- rownames(sample_1)[!(rownames(sample_1) %in% rownames(sample_2))]
+
+library(dplyr)
+gene_num <- filter(.data=ClassFreq, !Class%in%c('1000', '0101', '0001'))
