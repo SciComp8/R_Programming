852495077e5c244ca76b197e677a3cbb2fe74c31
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-06-03 17:28:06 -0400

    Update Delete_File_Directory.R

--- Convenient_R/File_Operation/Delete_File_Directory.R
+++ Convenient_R/File_Operation/Delete_File_Directory.R
@@ -3,3 +3,7 @@ output_file_name = '~/Downloads/Lab10_eQTL'
 unlink(output_file_name)
 
 unlink("~/Downloads/Lab7_scRNAseq/lab7_cache", recursive = T)
+# When deleting a directory (folder), "recursively" means removing:
+# The directory itself
+# All files inside it
+# All subdirectories and their contents
