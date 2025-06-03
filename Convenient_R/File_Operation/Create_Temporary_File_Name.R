289e9df45ca8c0b2e892240f6b94ae67202282c4
SciComp8 <90419301+SciComp8@users.noreply.github.com>
2025-06-03 16:25:33 -0400

    Create Create_Temporary_File_Name.R

---
+++ Convenient_R/File_Operation/Create_Temporary_File_Name.R
@@ -0,0 +1,4 @@
+tempfile(pattern = 'tmp', tmpdir = tempdir(), fileext = '')
+# pattern: a character vector suggesting the initial part of the name.
+# tmpdir: a character vector suggesting the directory name.
+# fileext: a character vector suggesting the file extension.
